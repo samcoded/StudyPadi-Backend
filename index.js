81b460fecb66f5b4e4acf1c4edcfb782e731f884
Samuel Anozie <samuelcanozie@gmail.com>
2021-04-26 22:26:48 +0100

    fix error routes

--- index.js
+++ index.js
@@ -4,19 +4,12 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const passport = require("passport");
+const welcome = require("./v1/controllers/welcome");
 
 dotenv.config();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
-//API version 1 routes
-const apiv1 = require("./v1/index");
-app.use("/api/v1", apiv1);
-
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
 
 //database connection
 const CONNECTION_URL = process.env.MONGOURL;
@@ -28,6 +21,22 @@ mongoose
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set("useFindAndModify", false);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//API version 1 routes
+const apiv1 = require("./v1/index");
+app.use("/api/v1", apiv1);
+app.get("/api", welcome);
+app.get("/", welcome);
+
+app.use((req, res, next) => {
+  return res
+    .status(404)
+    .json({ success: false, message: "ERROR: Wrong API ROUTE", data: {} });
+});
 
 //server listen
 app.listen(PORT, () =>
